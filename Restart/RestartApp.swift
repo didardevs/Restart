1c2eebf536d3f15e7d8da5ab01a8ea4c6aef1063
Didar Naurzbayev <didardevs@gmail.com>
2021-12-27 20:03:54 -0800

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Didar Naurzbayev on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
